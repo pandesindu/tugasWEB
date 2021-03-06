7ea69151165704315577073a3e42945e102d21ba
pandesindu <pande191909@gmail.com>
2021-03-07 00:00:37 +0800

    test

---
+++ index.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>sindu ardinata</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+ 
+<header>
+    <div class="container">
+    <div class="logo">
+        <img src="source/img/logo.png">
+    </div>
+    <div class="brand">
+        <h1>sindu ardinata</h1>
+    </div>
+    <nav>
+        <ul>
+            <li class="active"> <a href="index.html">beranda</a></li>
+            <li><a href="#">profil</a></li>
+            <li><a href="#">kontak</a></li>
+            <li><a href="java/register_form.php">Register</a></li>
+            <li><a href="#">sign in</a></li>
+            <form >
+            <input type="name" placeholder="search something">
+            <button class="buttom">search</button>
+        </form>
+        </ul>
+    </nav>
+    </div>
+</header>
+
+
+<section id="mainpage">
+    <div class="container">
+        <h1> promosi iklan dan berita </h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora nemo quas accusamus vel nobis ea nostrum magni? 
+            Minima saepe officiis provident exercitationeplaceat tenetur facerm totam e aperiam.
+             Saepe, repellendus libero.</p>
+     </div>
+</section>
+
+
+<section id="boxes">
+    <div class="container">
+        <div class="box">
+            <img src="source/img/gambar1.png">
+            <h3>konten 1</h3>
+            <p>lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora nemo quas accusamus vel nobis ea nostrum magni? 
+            Minima saepe officiis provident exercitationeplaceat tenetur facerm totam e aperiam.
+             Saepe, repellendus libero.</p>
+        </div>
+        <div class="box">
+            <img src="source/img/gambar2.png">
+            <h3>konten 2</h3>
+            <p>lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora nemo quas accusamus vel nobis ea nostrum magni? 
+            Minima saepe officiis provident exercitationeplaceat tenetur facerm totam e aperiam.
+             Saepe, repellendus libero.</p>
+        </div>
+        <div class="box">
+            <img src="source/img/gambar3.png">
+            <h3>konten 3</h3>
+            <p>lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora nemo quas accusamus vel nobis ea nostrum magni? 
+            Minima saepe officiis provident exercitationeplaceat tenetur facerm totam e aperiam.
+             Saepe, repellendus libero.</p>
+        </div>
+ 
+    </div>
+</section>
+ 
+
+<footer>
+    <h3>@copyrirgt by sindu_ardinata 2021</h3>
+</footer>
+
+ 
+</body>
+</html>
---
+++ java/register_form.php
@@ -0,0 +1,140 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Form Registrasi</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+  <style>
+  body {
+    background-color: #dcdcdc;
+  }
+  </style>
+</head>
+
+<body>
+
+<div class="container">
+  <h2>Form Registrasi </h2>
+  <form class="form-horizontal" action="/action_page.php">
+ 
+    <div class="form-group">
+      <label class="control-label col-sm-2">Nama:</label>
+      <div class="col-sm-10">
+        <input type="text" class="form-control" id="nama" placeholder="Masukan Nama" name="nama">
+      </div>
+    </div>
+    
+    <div class="form-group">
+      <label class="control-label col-sm-2">NIK:</label>
+      <div class="col-sm-10">
+        <input type="number" class="form-control" id="nik" placeholder="Masukan NIK" name="nik">
+      </div>
+    </div>
+    
+
+    <div class="form-group">
+      <label class="control-label col-sm-2" for="email">Email:</label>
+      <div class="col-sm-10">
+        <input type="email" class="form-control" id="email" placeholder="Masukan email" name="email">
+      </div>
+    </div>
+
+    <div class="form-group">
+                <label class="control-label col-sm-2" for="agama_mhs">Agama :</label>
+                <div class="col-sm-10"><select id="agama_mhs" class="form-control select2" style="width: 100%;">
+                    <option value="-" selected="selected">none</option>
+                    <option value="muslim">Muslim</option>
+                    <option value="kristen">Kristen</option>
+                    <option value="hindu">Hindu</option>
+                    <option value="budha">Budha</option>
+                    <option value="konghucu">Konghucu</option>
+                    <option value="lainnya">Lainnya</option>
+                </select>
+                </div>
+     </div>
+
+     <div class="form-group">
+                <label class="control-label col-sm-2" for="agama_mhs">Pekerjaan :</label>
+                <div class="col-sm-10"><select id="pekerjaan" class="form-control select2" style="width: 100%;">
+                    <option value="-" selected="selected">none</option>
+                    <option value="pns">PNS</option>
+                    <option value="wirausha">Wirausaha</option>
+                    <option value="mahasiswa">Mahasiswa</option>
+                    <option value="petani">Petani</option>
+                    <option value="lainnya">Lainnya</option>
+                </select>
+                </div>
+     </div>
+
+    <div class="form-group">
+      <label class="control-label col-sm-2" for="pwd">Password:</label>
+      <div class="col-sm-10">          
+        <input type="password" class="form-control" id="pwd" placeholder="Masukan password" name="pwd">
+      </div>
+    </div>
+
+    <div class="form-group">
+      <label class="control-label col-sm-2" for="pwd">Reenter Password:</label>
+      <div class="col-sm-10">          
+        <input type="password" class="form-control" id="repwd" placeholder="Masukan password lagi" name="repwd">
+      </div>
+        <div class = "errmsg"> </div>
+    </div>
+
+    <div class="form-group">        
+      <div class="col-sm-offset-2 col-sm-10">
+        <div class="checkbox">
+          <label><input type="checkbox" name="remember"> Remember me</label>
+        </div>
+      </div>
+    </div>
+
+
+    <div class="form-group">        
+      <div class="col-sm-offset-2 col-sm-10">
+        <button type="submit" class="btn btn-default"> Register Now</button>
+      </div>
+    </div>
+
+  <div class = "pesan">
+    <p></p>
+  </div>
+
+
+  </form>
+</div>
+</body>
+<script>
+
+  $(document).ready(function() {
+
+    $('h2').css('text-align', 'center');
+    $('h2').css('text-transform', 'uppercase');
+    $('h2').css('font-weight', 'bold');
+
+
+      $(".form-horizontal ").submit(function() {
+      var ps1 = $('#pwd').val();
+      var ps2 = $('#repwd').val(); 
+      var valid = 0;
+
+        if (ps1 == ps2) {
+           valid = 1;
+        } else {
+            $('.errmsg').html('Invalid password');
+        }
+        if (valid == 0) {
+           event.preventDefault();
+        } else {
+            $('p').html("Selamat pendaftaran  " + $('input[name="nama "]').val() + " telah berhasil ");
+         }
+      });
+  });
+
+
+</script>
+
+</html>
